087c2b1f4c0f7e76e5b0aea64777ac85ed83ffdd
squareman77 <75134215+squareman77@users.noreply.github.com>
2022-09-09 12:36:07 -0400

    Update branchingdemo.js

--- branchingdemo.js
+++ branchingdemo.js
@@ -1,4 +1,10 @@
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 const d = new Date();
 let month = months[d.getMonth()]
-console.log('The date is '+  d)
+let day = d.getDate();
+let year = d.getFullYear();
+
+let currentDate = "The current date is " + month + " " + day + ", " + year
+
+console.log(d)
+console.log(currentDate)
