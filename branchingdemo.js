e697862f0f9729bb760e888811fdfa588a554c86
squareman77 <75134215+squareman77@users.noreply.github.com>
2022-09-09 12:24:33 -0400

    Create branchingdemo.js

---
+++ branchingdemo.js
@@ -0,0 +1,4 @@
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const d = new Date();
+let month = months[d.getMonth()]
+console.log('The date is '+  d)
